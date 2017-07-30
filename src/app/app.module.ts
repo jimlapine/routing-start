2e92d4b61b2c5f8e08f854ea9f12c9d31b18b2ab
jimlapine <jim.lapine@gmail.com>
2017-07-30 09:38:47 -0400

    move routing to routing module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,8 +2,6 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
-// Import for Routing
-import { Routes, RouterModule } from '@angular/router';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
 import { UsersComponent } from './users/users.component';
@@ -12,23 +10,9 @@ import { UserComponent } from './users/user/user.component';
 import { EditServerComponent } from './servers/edit-server/edit-server.component';
 import { ServerComponent } from './servers/server/server.component';
 import { ServersService } from './servers/servers.service';
-
-// Routes constant
-const appRoutes: Routes = [
-  { path: '', component: HomeComponent },
-  // Declare users and child paths
-  // Router outlet is declared in users.component.html
-  { path: 'users', component: UsersComponent, children: [
-    // : tells angular this is the dynamic part of the path
-    { path: ':id/:name', component: UserComponent }
-  ] },
-  // Declare server path and it's child paths
-  // Router outlet declare in servers.component.html
-  { path: 'servers', component: ServersComponent, children: [
-    { path: ':id', component: ServerComponent },
-    { path: ':id/edit', component: EditServerComponent }
-  ] }
-];
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
+// Include our app routing module
+import { AppRoutingModule } from './app-routing.module';
 
 @NgModule({
   declarations: [
@@ -38,14 +22,15 @@ const appRoutes: Routes = [
     ServersComponent,
     UserComponent,
     EditServerComponent,
-    ServerComponent
+    ServerComponent,
+    PageNotFoundComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
     HttpModule,
-    // Register defined routes with Angular
-    RouterModule.forRoot(appRoutes)
+    // Include our app routing module
+    AppRoutingModule
   ],
   providers: [ServersService],
   bootstrap: [AppComponent]
